6e90a6f1161e9bc3aa8f473b6c66eadad438568b
otakuyoyo <97350376+otakuyoyo@users.noreply.github.com>
2024-01-09 03:34:05 +0000

    新增1.c

---
+++ downloads/ANSIC/1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+// Print Name
+printf("Name :謝承祐\n");
+// Print Date of Birth
+printf("DOB : December 29, 2004\n");
+
+// Print Mobile Number
+printf("Mobile : 09-58730523\n");
+
+// Indicate successful execution
+return(0);
+}
